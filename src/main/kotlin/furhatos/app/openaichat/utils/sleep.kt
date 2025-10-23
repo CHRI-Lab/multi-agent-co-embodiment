bc83e56208b3be9213e923ad4f53ca1198736d5a
Alex Wang <Alex226336@gmail.com>
2025-10-24 01:53:38 +1100

    change ilde timer

--- src/main/kotlin/furhatos/app/openaichat/utils/sleep.kt
+++ src/main/kotlin/furhatos/app/openaichat/utils/sleep.kt
@@ -10,7 +10,7 @@ class IdleTimeout : Event()
 private val idleScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 private val idleGen = AtomicInteger(0)
 
-fun FlowControlRunner.resetIdleTimer(timeoutMs: Long = 10_000) {
+fun FlowControlRunner.resetIdleTimer(timeoutMs: Long = 600_000) {
     val myGen = idleGen.incrementAndGet()
     println("reset IdleTimer")
 
